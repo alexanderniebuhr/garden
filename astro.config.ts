1572359e8349a9e972ef116bd61360271db7941c
Alexander Niebuhr <alexander@nbhr.io>
2026-02-05 23:14:07 +0100

    update to astro v6

--- astro.config.ts
+++ astro.config.ts
@@ -1,63 +1,46 @@
-import cloudflare from '@astrojs/cloudflare';
-import markdoc from '@astrojs/markdoc';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import expressiveCode from 'astro-expressive-code';
-import browserslist from 'browserslist';
-import { browserslistToTargets } from 'lightningcss';
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import browserslist from "browserslist";
+import { browserslistToTargets } from "lightningcss";
 
 export default defineConfig({
-	site: 'https://alexanderniebuhr.me',
-	prefetch: {
-		prefetchAll: true,
+	site: "https://alexanderniebuhr.garden",
+	output: "server",
+	adapter: cloudflare({
+		imagesBindingName: "cloudflare-binding",
+	}),
+	compressHTML: false,
+	experimental: {
+		contentIntellisense: true,
+		svgo: true,
+		chromeDevtoolsWorkspace: true,
 	},
-	security: {
-		checkOrigin: true,
+	integrations: [expressiveCode()],
+	devToolbar: {
+		enabled: false,
 	},
-	env: {
-		schema: {},
-	},
-	integrations: [
-		expressiveCode(),
-		markdoc({
-			allowHTML: true,
-		}),
-		sitemap(),
-	],
-	output: 'static',
-	adapter: cloudflare(),
 	vite: {
 		css: {
-			transformer: 'lightningcss',
+			transformer: "lightningcss",
 			lightningcss: {
 				targets: browserslistToTargets(
-					/**
-					 * https://browsersl.ist/
-					 * https://github.com/browserslist/browserslist
-					 */
 					browserslist([
-						'> 0.5%',
-						'last 2 versions',
-						'Firefox ESR',
-						'not dead',
-						'cover 80% in CN',
-						'unreleased versions',
+						"> 0.5%",
+						"last 2 versions",
+						"Firefox ESR",
+						"not dead",
+						"cover 80% in CN",
+						"unreleased versions",
 					]),
 				),
-				drafts: {
-					customMedia: true,
-				},
 			},
 		},
 		build: {
 			minify: false,
 			cssMinify: false,
 		},
-	},
-	compressHTML: false,
-	experimental: {
-		contentIntellisense: true,
-		liveContentCollections: true,
-		preserveScriptOrder: true,
+		plugins: [tailwindcss()],
 	},
 });
